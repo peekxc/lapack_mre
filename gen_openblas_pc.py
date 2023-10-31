765e64d90e8d2922bd9b7af13ad0b8e06a8ecb7a
peekxc <matt.piekenbrock@gmail.com>
2023-10-31 17:07:53 -0400

    Adding standard CI workflows

--- gen_openblas_pc.py
+++ gen_openblas_pc.py
@@ -1,6 +1,13 @@
 import os 
 import importlib
 
+def scipy_build_config():
+  import scipy as sp
+  config = sp.show_config(mode="dicts")
+  m_info = config['Machine Information']
+  b_info = config['Build Dependencies']
+  return b_info
+
 if __name__ == '__main__':
 
   print("Detecting scipy openblas")
@@ -16,6 +23,9 @@ if __name__ == '__main__':
   pkg_config_fname = os.path.join(openblas_dir, "scipy-openblas.pc")
   os.makedirs(openblas_dir, exist_ok=True)
 
+  ## Get SciPy build configuration
+  b_info = scipy_build_config()
+
   ## Write the pkgconfig file
   openblas = importlib.import_module(f"scipy_openblas{blas_variant}")
   pkg_config = openblas.get_pkg_config().split('\n')
@@ -23,6 +33,14 @@ if __name__ == '__main__':
     if config_str[:5].upper() == 'LIBS:' and len(config_str) <= 6:
       config_str += r"-L${libdir}"
       pkg_config[i] = config_str
+    elif config_str[:8].upper() == 'EXTRALIB':
+      extra_lib = pkg_config[i]
+      pkg_config[i] += f" -L{b_info['blas']['lib directory']}"
+      pkg_config[i] += f" -L{b_info['lapack']['lib directory']}"
+    elif config_str[:6].upper() == 'CFLAGS':
+      pkg_config[i] += f" -I{b_info['blas']['include directory']}"
+      pkg_config[i] += f" -I{b_info['lapack']['include directory']}"
+
   pkg_config = '\n'.join(pkg_config)
   with open(pkg_config_fname, "wt", encoding="utf8") as fid:
     fid.write(pkg_config.replace("\\", "/"))
